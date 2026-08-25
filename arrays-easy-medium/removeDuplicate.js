075d0f3c76f80749bde3555a01757e2852919230
Mimansha Swarup <mimanshaswarup@gmail.com>
2026-08-25 10:51:01 +0530

    refactor: update removeDuplicate to return modified array and count by adjusting iteration logic

--- arrays-easy-medium/removeDuplicate.js
+++ arrays-easy-medium/removeDuplicate.js
@@ -18,13 +18,13 @@ Output: 2, nums = [1,2,_]
 const removeDuplicate = (nums) =>{
     let i = 0;
     let j =0;
-    for(j=1;j<nums.length;j++){
-        if(nums[j]!==nums[i]){
+    for(j= 0;j<nums.length;j++){
+        if(nums[i]<nums[j]){
             i++;
             nums[i]=nums[j];
         }
     }
-    return i+1
+    return [nums, i+1]
     
 
 }
